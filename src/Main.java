c4f0de50f3aa7b3037ec464da63bec2d2d8fecf6
Abdullah200305 <112935137+Abdullah200305@users.noreply.github.com>
2026-01-23 10:25:38 +0100

    done

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,26 @@
 import Redovisning1.Uppgift3;
 
+import java.util.Iterator;
 public class Main {
     public static void main(String[] args) {
         Uppgift3<Integer> list = new Uppgift3<>();
-        System.out.println(list.Add(10));
-        System.out.println(list.Add(20));
-        System.out.println(list.Add(30));
-        System.out.println(list.Add(40));
+        list.Add(10);
+        list.Add(20);
+        list.Add(30);
+        list.Add(50);
+        list.Add(60);
+        Iterator ite = list.iterator();
+        while (ite.hasNext()){
+            System.out.println(ite.next());
+            ite.remove();
+
+        }
+        System.out.println(list.toString());
+
+
+
+
+
     }
 }
 
--- src/Redovisning1/Uppgift3.java
+++ src/Redovisning1/Uppgift3.java
@@ -1,6 +1,8 @@
 package Redovisning1;
 
+import java.util.EmptyStackException;
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 public class Uppgift3<E> {
     private Node head;
@@ -13,9 +15,54 @@ public class Uppgift3<E> {
             this.next = next;
         }
     }
+    private class Ite implements Iterator<E>{
+        Node<E> current;
+        Node<E> lastReturned;
+        Node<E> prev;
+        public Ite(Node<E> start){
+            current = start;
+        }
+        @Override
+        public boolean hasNext() {
+            return current!=null;
+        }
+
+        @Override
+        public E next() {
+            if (current == null) {
+                throw new NoSuchElementException();
+            }
+            prev = lastReturned;
+            lastReturned = current;
+            current = current.next;
+            return lastReturned.data;
+        }
+
+        @Override
+        public void remove() {
+            if (lastReturned == null) {
+                throw new IllegalStateException("next() not called or already removed");
+            }
+
+            if (prev == null) {
+                head = current;
+            } else {
+                prev.next = current;
+            }
+            size--;
+            lastReturned = null;
+        }
+    }
+
+
+
     public Uppgift3(){
         head = null;
         size = 0;
+    }
+
+    public Iterator<E> iterator(){
+        return new Ite(head);
     }
     public boolean Add(E item,int index){
         if(head==null){
@@ -29,11 +76,12 @@ public class Uppgift3<E> {
     public boolean Add(E item){
         if(head==null){
             AddFirst(item);
-            System.out.println(head.data);
         }
         else{
-            AddAfter(0,item);
+           Node<E> node = getNode(size-1);
+           node.next = new Node<>(item,null);
         }
+        size++;
         return true;
     }
     private boolean AddFirst(E item){
@@ -41,19 +89,54 @@ public class Uppgift3<E> {
         return true;
     }
     private boolean AddAfter(int index,E item){
-        Node current = getNode(index);
-        current = new Node<>(item,null);
+        if(index==0){
+            head = new Node<>(item,head);
+        }
+        else {
+            Node prev = getNode(index-1);
+            prev.next = new Node<>(item, prev.next);
+        }
         return true;
     }
+
+
     private Node<E>getNode(int index){
-        if(index<0 || index>= size){
-            throw new ArrayIndexOutOfBoundsException(index);
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException(index);
         }
+
         Node<E> current = head;
-        for (int i = index; i < size && current.next != null; i++) {
-            System.out.println(current);
+        for (int i = 0; i < index; i++) {
             current = current.next;
         }
         return current;
+    }
+
+
+
+
+
+
+
+    public int size() {
+        return size;
+    }
+
+
+
+    @Override
+    public String toString(){
+        StringBuilder str = new StringBuilder();
+        if(size==0){
+            str.append("[]");
+        }
+        Iterator<E> ite = this.iterator();
+        while (ite.hasNext()){
+            str.append(ite.next()+" ");
+            if(ite.hasNext()){
+                str.append("=> ");
+            }
+        }
+        return str.toString();
     }
 }
