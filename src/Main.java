368d3226488b3ba6cdbcd7dff12ab377b042ed92
Abdullah200305 <112935137+Abdullah200305@users.noreply.github.com>
2026-02-17 10:15:02 +0100

    Redovisning 3

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,11 @@
-import lektion9.MyProgram;
-import lektion9.NB28;
-import lektion9.Shellsort;
-import lketion10.Heap;
-import lketion10.NB30;
-import test.CountingSort;
-import test.MaxHeap;
-
+import Redovisning3.Game;
 
 public class Main{
     public static void main(String[] args) {
-        Heap heap = new Heap();
-        int[] list = {22,4,3,-1,-2,3,3,2,21,1,1,3,52,1};
-        heap.Heapsort(list);
-
-
-
-        StringBuilder str = new StringBuilder();
-        for (int i = 0; i < list.length; i++) {
-            str.append(list[i]+" ");
-        }
-        System.out.println(str.toString());
+        Game run = new Game();
+        run.mapFill(1,3);
+        run.FillRun();
+        System.out.println(run.FillRun());
     }
 
 }
---
+++ src/Redovisning3/Game.java
@@ -0,0 +1,106 @@
+package Redovisning3;
+
+
+public class Game {
+    private final int row = 5;
+    private final int col = 5;
+    private int[][] map =  new int[row][col];
+    int solutions = 0;
+
+    int[][][] shapes = {
+            { {0,0}, {1,0}, {0,1} },
+            { {0,0}, {0,1}, {1,1} },
+            { {0,0}, {1,0}, {1,1} },
+            { {0,0}, {1,0}, {1,-1} }
+    };
+
+
+
+
+    public void mapFill(int postionRow,int postionCol){
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                map[i][j]= 0;
+            }
+        }
+        map[postionRow-1][postionCol-1]=1;
+    }
+
+
+
+    public int FillRun(){
+        AntalSolveTime(0);
+        return solutions;
+    }
+    private void AntalSolveTime(int antal){
+        if(antal==8){
+            solutions++;
+
+            return;
+        }
+        int pos[] = findEmpty();
+        if(pos==null)return;
+        for (int i = 0; i < 4; i++) {
+             if(CanPlace(pos,shapes[i])){
+                palce(pos,shapes[i]);
+                AntalSolveTime(antal+1);
+                remove(pos,shapes[i]);
+             }
+            }
+
+
+    }
+
+    private void remove(int[] pos, int[][] shape){
+        for (int i = 0; i < shape.length; i++) {
+            int nx = pos[1] + shape[i][0];
+            int ny = pos[0] + shape[i][1];
+            map[ny][nx] = 0;
+        }
+    }
+    private void palce(int[] pos, int[][] shape){
+
+        for (int i = 0; i < shape.length; i++) {
+            int nx = pos[1] + shape[i][0];
+            int ny = pos[0] + shape[i][1];
+            map[ny][nx] = 1;
+        }
+    }
+
+    private boolean CanPlace(int[] pos, int[][] shape){
+        for (int i = 0; i < shape.length; i++) {
+            int nx = pos[1] + shape[i][0];
+            int ny = pos[0] + shape[i][1];
+
+            if (nx < 0 || nx >= col || ny < 0 || ny >= row) return false;
+            if (map[ny][nx] != 0) return false;
+        }
+        return true;
+    }
+    private int[] findEmpty(){
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                if(map[i][j]==0){
+                    return new int[]{i,j};
+                }
+            }
+        }
+        return null;
+    }
+
+
+
+
+
+    @Override
+    public String toString() {
+       StringBuilder show = new StringBuilder();
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                show.append(map[i][j]+" ");
+            }
+            show.append("\n");
+        }
+        return show.toString();
+    }
+}
